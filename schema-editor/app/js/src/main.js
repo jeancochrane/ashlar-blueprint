18b194ccbd9b1b55fcdebf2038e085d8ba745476
Jean Cochrane <jean@jeancochrane.com>
2018-06-12 12:28:39 -0400

    Sketch out schema editor ES6 app architecture using yarn + webpack

---
+++ schema-editor/app/js/src/main.js
@@ -0,0 +1,1 @@
+// main.js -- primary JS file for the Ashlar schema editor app
---
+++ schema-editor/app/webpack.common.config.js
@@ -0,0 +1,91 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var outputPath = '/usr/dist';
+
+module.exports = {
+    entry: {
+        schema-editor: ['babel-polyfill', './js/src/main.js'],
+        vendor: [
+            'axios',
+        ],
+    },
+    output: {
+        path: outputPath,
+        publicPath: '/',
+        filename: '[name].bundle.[hash].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Ashlar Blueprint Schema Editor',
+            filename: 'templates/schema-editor.html'
+            template: 'schema-editor.html',
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|lib)/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.js?/,
+                exclude: /(node_modules|lib|json)/,
+                loader: 'eslint-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'resolve-url-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [require('autoprefixer')]
+                        }
+                    },
+                ],
+            },
+            {
+                test: /fonts.*\.(woff|woff2|ttf|eot|svg)($|\?)/,
+                loader: 'url-loader?limit=25000&name=font/[name].[ext]',
+            },
+            {
+                test: /(img|images).*\.(jpg|png|gif|svg)$/,
+                loader: 'url-loader?limit=25000&name=img/[name].[ext]',
+            },
+            {
+                test: /.*\.(json)$/,
+                exclude: /node_modules/,
+                loader: 'json-loader',
+            },
+            {
+                test: /\.(html)$/,
+                loader: 'html-loader?name=[name].[ext]',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+        modules: ['common', 'node_modules', 'img', 'js'],
+    },
+    node: {
+        fs: 'empty',
+    },
+    devServer: {
+        disableHostCheck: true,
+        historyApiFallback: {
+            index: '/',
+        }
+    },
+};
